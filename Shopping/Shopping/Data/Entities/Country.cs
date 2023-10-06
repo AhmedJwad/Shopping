cba0fc9fe077ae33ba61e2ca77b2907a450a6381
AhmedJwad <Ahmednet380@gmail.com>
2023-10-05 22:45:52 -0800

    Add database

---
+++ Shopping/Shopping/Data/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.Data.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Country")]
+        [MaxLength(100, ErrorMessage = "The field {0} must have a maximum of {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is required.")]
+        public string Name { get; set; }
+    }
+}
